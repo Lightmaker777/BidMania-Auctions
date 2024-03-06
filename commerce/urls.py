2cc59571191c11706ee6bdd57510e859b91f6f87
Lightmaker777 <j.kochkorian@gmx.de>
2024-03-06 19:49:20 +0100

    update commerce/urls.py

--- commerce/urls.py
+++ commerce/urls.py
@@ -35,7 +35,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("auctions.urls")),
-    path("api/", include("auctions.urls")),
+    
 
     path('openapi/',schema_view.without_ui(cache_timeout=0)),
     path('swagger/',schema_view.with_ui('swagger',cache_timeout=0),name='schema-swagger-ui'),
